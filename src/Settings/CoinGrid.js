83a0b38fec1b7d418674fdc37290a6a0fc777fe3
GWilczyk <gwilczyk.github@gmail.com>
2020-11-16 23:52:45 +0100

    Responsive Grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -5,14 +5,13 @@ import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
 	display: grid;
-	grid-template-columns: repeat(5, 1fr);
+	grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
 	grid-gap: 15px;
 	margin-top: 40px;
 `;
 
 const getCoinsToDisplay = (coinList, favorites, topSection) => {
 	return topSection ? favorites : Object.keys(coinList).slice(0, 100);
-	//	return Object.keys(coinList).slice(0, topSection ? 10 : 100);
 };
 
 const CoinGrid = ({ topSection }) => {
